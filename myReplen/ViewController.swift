0f129d96f17e7689955af8d62722793c61f31337
Ernest288 <ernest.thu@gmail.com>
2017-11-28 21:28:25 -0500

    Initial Commit

---
+++ myReplen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myReplen
+//
+//  Created by Ernest Thu on 2017-11-28.
+//  Copyright © 2017 Ernest Thu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
